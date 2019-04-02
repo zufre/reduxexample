c1c7493289989f741c7be8cb39ae1f54b8b55fd7
Cédric David <cedric.david@btopenworld.com>
2019-04-01 22:07:50 -1100

    update files

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 export class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
   componentWillMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
   render() {
     const postItems = this.state.posts.map(post => (
@@ -28,4 +22,7 @@ export class Posts extends Component {
   }
 }
 
-export default Posts;
+export default connect(
+  null,
+  { fetchPosts }
+)(Posts);
